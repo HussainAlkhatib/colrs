1a45f4d6a0358a830c52d9df233c68ce2e6e17e5
HussainAlkhatib <h2311065@gmail.com>
2026-03-10 23:47:39 +0100

    Bump version to 0.5.0

--- colrs/__init__.py
+++ colrs/__init__.py
@@ -4,7 +4,7 @@
 colrs - A simple and elegant way to color your terminal text.
 """
 
-__version__ = "0.4"
+__version__ = "0.5.0"
 
 # Initialize colorama to make ANSI codes work on Windows and to auto-reset colors.
 from colorama import init
